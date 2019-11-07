0f369e83bccfd03bcaf9c4d751478cebbfdcd120
Dinaerte Neto <dinaerte_de_assis_neto@carrefour.com>
2019-11-07 17:56:56 -0300

    add middlewares

--- modulo01/index.js
+++ modulo01/index.js
@@ -10,20 +10,43 @@ server.use(express.json());
 
 const users = ['Dinaerte', 'Leonardo', 'Renato'];
 
-server.get('/users', (req, res) => res.json(users));
+server.use((req, res, next) => {
+  console.time('REQUEST');
+  console.log(`MIDDLEWARE GLOBAL - Método: ${req.method}; URL: ${req.url}`);
+  next();
+  console.timeEnd('REQUEST');
+});
 
-server.post('/users', (req, res) => {
+function checkUserExists(req, res, next) {
+  if(!req.body.name) {
+    return res.status(400)
+      .json({ error: 'User name is required.'});
+  }
+  return next();
+}
+
+function checkUserInArray(req, res, next) {
+  const user = users[req.params.index];
+  if(!user) {
+    return res.status(400)
+    .json({ error: 'User does not exists.'});    
+  }
+  req.user = user;
+  return next();
+}
+
+server.get('/users', checkUserInArray, (req, res) => res.json(users));
+server.get('/users/:index', checkUserInArray, (req, res) => {
+  return res.json(req.user);
+});
+
+server.post('/users', checkUserExists, (req, res) => {
   const { name } = req.body;
   users.push(name);
   return res.json(users);
 });
 
-server.get('/users/:index', (req, res) => {
-  const { index } = req.params;
-  return res.json(users[index]);
-});
-
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUserInArray, checkUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
 
@@ -32,7 +55,7 @@ server.put('/users/:index', (req, res) => {
   return res.json(users);
 });
 
-server.delete('/users/:index', (req, res) => {
+server.delete('/users/:index', checkUserInArray, (req, res) => {
   const { index } = req.params;
   users.splice(index, 1);
 
